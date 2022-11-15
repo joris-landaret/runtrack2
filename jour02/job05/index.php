0a941af9386d3addf2c2e679daf4d8ff5ae72829
joris-landaret <joris.landaret@laplateforme.io>
2022-11-15 15:28:58 +0100

    job05 du jour 2 fini

---
+++ jour02/job05/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$x = 0;
+
+echo "2<br>";
+
+while ( $x <= 1000) {
+    $x++;
+     
+    for ($i = 2; $i <= $x; $i++){
+    
+        if ( ($x % $i) == 0 ){break;} // si x divisible par i alors il n'est pas premier et on ne veut pas l'aficher
+        if ($i == ($x-1)){ echo "$x<br>";}
+        
+    }
+    
+}
+
+?>
