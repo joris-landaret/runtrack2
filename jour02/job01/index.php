0c1ae0ea84f0e5635173df37a1d13964331e3e08
joris-landaret <joris.landaret@laplateforme.io>
2022-11-15 11:41:00 +0100

    job01 du jour 2 fait

---
+++ jour02/job01/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$x = 0;
+
+while ( $x <= 1337) {
+    
+    if( $x == 42 ){
+        echo "<b><u>$x</u></b>";
+        }
+
+    else{
+        echo $x;
+    }
+    echo "<br>";
+    $x++;
+}
+
+?>
