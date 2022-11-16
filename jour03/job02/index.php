c22d161be07e98a644e5cb891f186ddf5e6416bc
joris-landaret <joris.landaret@laplateforme.io>
2022-11-16 13:08:48 +0100

    jour03, job02 fini

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -1,5 +1,14 @@
 <?php
 
+$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
+$i = 0;
+//$i = $i + 1;
 
+while(isset($str[$i])){
+    
+    echo $str[$i++];
+    $i++;
+
+}
 
 ?>
