30e3af97a030861c471c1b0af140402aea43544d
joris-landaret <joris.landaret@laplateforme.io>
2022-11-17 16:29:55 +0100

    jour04, job04 fini

--- jour04/job04/index.php
+++ jour04/job04/index.php
@@ -1,5 +1,64 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>formulaire</title>
+    <link href="css/grèce.css" rel="stylesheet" type="text/css">
+  </head>
+
+  <body>
+
+    <form action="" method="post">
+        <label for="nom">Quel est votre nom:</label>
+        <input type="text" name="nom" >
+
+        <label for="prenom">Quel est votre prenom:</label>
+        <input type="text" name="prenom" >
+
+        <input type="submit" value="Envoyer" >
+    </form>
+    <table border=3>
+
+<thead>
+    <tr>
+        <th>Argument</th>
+        <th>Valeur</th>
+        
+    </tr>
+</thead>
+
+
+
+
+
+
 <?php
 
+var_dump($_POST);
 
+
+foreach($_POST as $key => $result){
+    echo "
+    <tr>
+                <td>$key</td>
+                <td>$result</td>
+                
+            </tr>";
+   
+}
+
+/*$i=0;
+
+while(isset($_GET[$i])){
+    $i++;
+}*/
+
+
+    
 
 ?>
+
+
+    </table>
+  </body>
+</html>
