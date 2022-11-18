b8a99729d86d372886e09fa54019fb1b5249f3f9
joris-landaret <joris.landaret@laplateforme.io>
2022-11-18 10:37:48 +0100

    jour04, job05 fini

--- jour04/job05/index.php
+++ jour04/job05/index.php
@@ -1,5 +1,33 @@
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>formulaire</title>
+    <link href="css/grèce.css" rel="stylesheet" type="text/css">
+  </head>
+
+  <body>
+
+    <form action="" method="post">
+        <label for="username">Quel est votre username:</label>
+        <input type="text" name="username" >
+
+        <label for="password">Quel est votre mot de passe:</label>
+        <input type="text" name="password" >
+
+        <input type="submit" value="connexion" >
+    </form>
+    <p></p>
+  </body>
+</html>
+
 <?php
 
-
+    if($_POST['username'] == "John" && $_POST['password'] == "Rambo"){
+        echo "“C’est pas ma guerre”";
+    }
+    else{echo "“Votre pire cauchemar”";}
 
 ?>
