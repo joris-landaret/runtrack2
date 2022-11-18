3f741f2dd26bca06916b621d9ec802c468612503
joris-landaret <joris.landaret@laplateforme.io>
2022-11-18 11:03:42 +0100

    jour04, job06 fini

--- jour04/job06/index.php
+++ jour04/job06/index.php
@@ -1,5 +1,30 @@
 <?php
 
+var_dump($_GET);
 
+if($_GET['nombre'] % 2 == 0){
+    echo "“Nombre pair”";
+}
+else {echo "“Nombre impair”";}
 
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>formulaire</title>
+    <link href="css/grèce.css" rel="stylesheet" type="text/css">
+  </head>
+
+  <body>
+
+    <form action="" method="get">
+        <label for="nombre">veuillez saisir un nombre:</label>
+        <input type="text" name="nombre" >
+
+        <input type="submit" value="Envoyer" >
+    </form>
+
+  </body>
+</html>
