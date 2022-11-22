2973a1bc2c13540c442843e0aecf5954223d4589
joris-landaret <joris.landaret@laplateforme.io>
2022-11-22 15:14:21 +0100

    jour07, job05 fini

--- jour07/job05/index.php
+++ jour07/job05/index.php
@@ -1,5 +1,22 @@
 <?php
 
+$bonjour = 'Bonjour comment tu vos bien ?';
+$o = 'o';
 
+function occurrences($str, $char){
+    $x = 0;
+    for ($i=0; isset($str[$i]); $i++) { 
+        if($str[$i] == $char){
+            
+            $x = $x+1;
+            
+        }
+        
+    }
+
+return $x;
+}
+
+echo occurrences($bonjour, $o);
 
 ?>
