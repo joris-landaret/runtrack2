df50428a8024c550fdc4a0a93f261c4ee808fa7b
joris-landaret <joris.landaret@laplateforme.io>
2022-11-22 13:45:12 +0100

    jour07, job04 fini

--- jour07/job04/index.php
+++ jour07/job04/index.php
@@ -1,5 +1,19 @@
 <?php
 
+$nbr1 = 25;
+$plus = '-';
+$nbr2 = 11;
 
+
+function calcul($a,$operation,$b){
+    if($operation == '+'){ return $a+$b;}
+    elseif($operation == '-'){ return $a-$b;}
+    elseif($operation == '*'){ return $a*$b;}
+    elseif($operation == '/'){ return $a/$b;}
+    elseif($operation == '%'){ return $a%$b;}
+
+}
+
+echo calcul($nbr1,$plus,$nbr2);
 
 ?>
