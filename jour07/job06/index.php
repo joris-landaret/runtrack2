b7005bc9ee4baa6820771a6d2ff4e1485599f124
joris-landaret <joris.landaret@laplateforme.io>
2022-11-22 17:24:42 +0100

    jour07, job06 fini

--- jour07/job06/index.php
+++ jour07/job06/index.php
@@ -1,7 +1,31 @@
 <?php
 
+$chain = 'Un lion averti en vaut deux';
+
+
 function leetSpeak($str){
+    $minus = ['a','b','e','g','l','s','t'];
+    $maj = ['A','B','E','G','L','S','T'];
+    $integer = [4,8,3,6,1,5,7];
+
+    // parcour la chaine de charactère de $str
+    for ($i=0; isset($str[$i]) ; $i++) { 
+        // je créé un compteur pour mes trois tableaux
+        for ($x=0; $x < 7; $x++){
+            // je regarde que dans la chain
+            if($str[$i] == $minus[$x] || $str[$i] == $maj[$x]){
+                $str[$i] = $integer[$x];
+
+                // var_dump($minus[$x]);
+                // var_dump($str[$i]);
+            }
+        }
+        //var_dump($str);
+    }
     
+    return $str;
 }
+
+echo leetSpeak($chain)
 
 ?>
