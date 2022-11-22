3e761637ea362461ee93f8c30474f86fc83b1775
joris-landaret <joris.landaret@laplateforme.io>
2022-11-22 11:36:54 +0100

    jour07, job02 fini

--- jour07/job02/index.php
+++ jour07/job02/index.php
@@ -1,5 +1,19 @@
 <?php
 
+$a = true;
+$b = false;
 
+function bonjour($jour){
+    if($jour == true){
+        echo "Bonjour";
+    }
+    else{
+        echo "Bonsoir";
+    }
+}
+
+bonjour($a);
+echo '<br>';
+bonjour($b);
 
 ?>
