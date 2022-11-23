bbde35b28829695bb22374cdd0f6211ee0570ab6
joris-landaret <joris.landaret@laplateforme.io>
2022-11-23 17:12:57 +0100

    jour08, job02 en cours

--- jour08/job02/index.php
+++ jour08/job02/index.php
@@ -1,5 +1,35 @@
 <?php
 
+var_dump($_POST);
 
+if(isset($_POST['reset'])){
+    $_COOKIE['nbvisites'] = 0;
+    var_dump($_COOKIE);
+}
+            
+if(isset($_COOKIE['nbvisites'])){
+    $_COOKIE['nbvisites']++;
+    echo 'Nombre de visites : '.$_COOKIE['nbvisites'];
+    var_dump($_COOKIE);
+}
+else{
+    setcookie('nbvisites',0, time()+3600);
+    //$_COOKIE['nbvisites'];
+}
 
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>visites</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="cours.css">
+    </head>
+    
+    <body>
+        <form action="" method="post">
+            <input type="submit" value="Reset" name='reset'> 
+        </form>
+    </body>
+</html>
