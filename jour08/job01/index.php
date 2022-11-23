86d57830d140b69857173e62bfb29378f226bd86
joris-landaret <joris.landaret@laplateforme.io>
2022-11-23 16:18:55 +0100

    jour08, job01 fini

--- jour08/job01/index.php
+++ jour08/job01/index.php
@@ -1,5 +1,39 @@
 <?php
 
+session_start();
+
+if(isset($_POST['reset'])){
+    $_SESSION['nbvisites'] = 0;
+    var_dump($_SESSION);
+}
+
+            
+if(isset($_SESSION['nbvisites'])){
+    $_SESSION['nbvisites']++;
+    echo 'Nombre de visites : '.$_SESSION['nbvisites'];
+    var_dump($_SESSION);
+}
+else{
+    $_SESSION['nbvisites'] = 0;
+}
+
+var_dump($_POST);
+
 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>visites</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="cours.css">
+    </head>
+    
+    <body>
+        <form action="" method="post">
+            <input type="submit" value="Reset" name='reset'> 
+        </form>
+    </body>
+</html>
